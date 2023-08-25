31d4b55cf186ccdee79467dae91d31c0031446b8
roniekun <roniebenitez01@gmail.com>
2023-08-25 19:27:05 +0800

    update

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,17 +1,70 @@
 import React from 'react'
 import Footer from '../Footer';
 import './styles/Home.css';
-import SiteLogo from '../SiteLogo';
+import { gsap } from 'gsap';
+import { useRef, useEffect, useState} from 'react';
+
+
 
 const Home = ({setShowNavbar}) => {
+
+  const [fontSize, setFontSize] = useState(); 
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+
+let xPercent = 0;
+
+useEffect( () => {
+  gsap.set(secondText.current, {left: secondText.current.getBoundingClientRect().width})
+  requestAnimationFrame(animate);
+}, [])
+
+const animate = () => {
+  if(xPercent > 0){
+    xPercent = -100;
+  }
+  gsap.set(firstText.current, {xPercent: xPercent})
+  gsap.set(secondText.current, {xPercent: xPercent})
+  requestAnimationFrame(animate);
+  xPercent += 0.1;
+}
+
+const calculateFontSize = () => {
+
+  const newFontSize =  window.innerWidth /20;
+  setFontSize(newFontSize);
+
+};
+useEffect(() => {
+  calculateFontSize();
+
+  window.addEventListener('resize', calculateFontSize);
+
+  return () => {
+    window.removeEventListener('resize', calculateFontSize);
+  };
+}, []);
+
   return (
     <div className='home_container'>
  
       <div className='content'>
       <div className='hero_container'>
+        <div   ref={slider} className='textslider_container'>
+          <div className='bg_text' ref={firstText}>
+          <h1  style={{ fontSize: `${fontSize}px` }}  >
+            pitik south cotabato </h1>
+          </div>
+          <div className='bg_text' ref={secondText} >
+          <h1 style={{ fontSize: `${fontSize}px` }} >
+            pitik south cotabato</h1>
+          </div>
+        </div>
         <h1>Hero</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
           Aperiam dignissimos enim quod consequatur ad eius.</p>
+          <button className='cta_button'>Call to action</button>
       </div>
 
       <div className='one'>
