54910670818a7ec61aad8715e3ed2a57b2672728
roniekun <roniebenitez01@gmail.com>
2023-09-04 18:11:27 +0800

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,8 +25,8 @@ const Footer = ({setShowNavbar}) => {
       footerContainer={{ }}/>
       <h3 style={{textTransform: 'uppercase',
                   fontSize: '16px'}}>All rights reserved 2023</h3>
-       <h4>Version 1.1</h4>
-       <a href="#">developed by roniekun</a>
+       <h4>version 1.1</h4>
+       <a style={{textDecoration: 'none', color: 'gray'}} href="#">developed by roniekun</a>
        <h4>www.pitiksouthcotabato.com</h4>
 
       </div>
